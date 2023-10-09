0f6d1bfd6a87a1008fea0af5b8d294a1781d26bb
Javeriya <javeriya.noorisys@gmail.com>
2023-10-09 16:10:36 +0530

    admin Profile

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\admin\AuthController;
+use App\Http\Controllers\api\admin\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['localization'])->group(function () {
+    
     // Admin APIs
     Route::prefix('admin')->group(function () {
         Route::post('login' , [AuthController::class, 'login']);
