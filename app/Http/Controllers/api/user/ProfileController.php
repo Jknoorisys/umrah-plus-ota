74cf742c5d2cdd74270f86bcf59811325ba1663f
Javeriya <javeriya.noorisys@gmail.com>
2023-10-10 11:51:45 +0530

    bug

--- app/Http/Controllers/api/user/ProfileController.php
+++ app/Http/Controllers/api/user/ProfileController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\api\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use function App\Helpers\AuthUser;
