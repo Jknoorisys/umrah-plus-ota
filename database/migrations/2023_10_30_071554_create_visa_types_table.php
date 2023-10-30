13d4299416176a2ace161532e39b6a494e58ecd9
Javeriya <javeriya.noorisys@gmail.com>
2023-10-30 13:04:41 +0530

    visa types

---
+++ database/migrations/2023_10_30_071554_create_visa_types_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visa_types', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('country_id');
+            $table->foreign('country_id')->references('id')->on('visa_countries')->onDelete('cascade');
+            $table->string('type');
+            $table->string('processing_time');
+            $table->string('stay_period');
+            $table->string('validity');
+            $table->string('entry');
+            $table->string('fees');
+            $table->string('currency');
+            
+            $table->enum('is_featured', ['yes', 'no'])->default('no');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visa_types');
+    }
+};
